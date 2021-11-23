dac82f8d8f045e9166852ed209819dc874735776
alvarosalamancax <89539681+alvarosalamancax@users.noreply.github.com>
2021-11-22 23:16:28 -0300

    MENUU

--- Taller3/Assets/Script/MenuManager.cs
+++ Taller3/Assets/Script/MenuManager.cs
@@ -1,16 +1,17 @@
-using System.Collections;
+  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
+    public int XR
  public void BotonStart()
     {
-        SceneManager.LoadScene("XR");
+        SceneManager.LoadScene(XR);
     }
     public void BotonQuit()
     {
-        Debug.Log("Quitamos la aplicacion");
+   
         Application.Quit();
     }
 }
